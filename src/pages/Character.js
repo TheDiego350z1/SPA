50ceff86cb33d756bbcedbadf96de6d6c5a6bda4
Diego Antonio García Rivas <diego350z1@gmail.com>
2022-01-21 12:15:09 -0600

    Add Character.js and Error404.js

---
+++ src/pages/Character.js
@@ -0,0 +1,22 @@
+const Character = () => {
+    const view = `
+        <div class="Character-inner">
+            <article class=""Characters-card>
+                <img src="img" alt="name" />
+                <h2>Name</h2>
+            </article>
+            <article class="Characters-card">
+                <h3>Episodios:</h3>
+                <h3>Estado:</h3>
+                <h3>Especie:</h3>
+                <h3>Genero:</h3>
+                <h3>Origin:</h3>
+                <h3>Ultima Hubicación:</h3>
+            </article>
+        </div>
+    `;
+
+    return view;
+};
+
+export default Character;
